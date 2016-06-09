6b033e4a7fb0e94c7b5d22826ce438a49a74d084
lfaler <laynefaler@gmail.com>
2016-06-08 18:25:34 -0700

    Minor changes to Contract

--- app/src/main/java/com/example/laynefaler/hotflix/Data/MovieDBContract.java
+++ app/src/main/java/com/example/laynefaler/hotflix/Data/MovieDBContract.java
@@ -25,34 +25,34 @@ public class MovieDBContract extends SQLiteOpenHelper {
         final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
         MovieEntry._ID + " INTEGER PRIMARY KEY," +
                 MovieEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
-                MovieEntry.COLUMN_IS_ADULT + " TEXT NOT NULL, " +
-                MovieEntry.COLUMN_BACK_DROP_PATH + " TEXT NOT NULL, " +
-                MovieEntry.COLUMN_ORIGINAL_LANGUAGE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_IS_ADULT + " TEXT, " +
+                MovieEntry.COLUMN_BACK_DROP_PATH + " TEXT, " +
+                MovieEntry.COLUMN_ORIGINAL_LANGUAGE + " TEXT, " +
                 MovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT NOT NULL," +
                 MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
                 MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
                 MovieEntry.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
                 MovieEntry.COLUMN_POPULARITY + " REAL NOT NULL, " +
-                MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
-                MovieEntry.COLUMN_IS_VIDEO + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_TITLE + " TEXT, " +
+                MovieEntry.COLUMN_IS_VIDEO + " TEXT, " +
                 MovieEntry.COLUMN_VOTE_AVERAGE + " FLOAT NOT NULL, " +
-                MovieEntry.COLUMN_VOTE_COUNT + " INTEGER NOT NULL, " +
-                MovieEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                MovieEntry.COLUMN_VOTE_COUNT + " INTEGER, " +
+                MovieEntry.COLUMN_DATE + " INTEGER, " +
                 MovieEntry.COLUMN_RUNTIME + " REAL, " +
                 MovieEntry.COLUMN_STATUS + " TEXT " +
                 " );";
 
         final String SQL_CREATE_TRAILER_TABLE = "CREATE TABLE " + MovieContract.TrailerEntry.TABLE_NAME + " (" +
                 MovieContract.TrailerEntry._ID + " INTEGER PRIMARY KEY," +
-                MovieContract.TrailerEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
-                MovieContract.TrailerEntry.COLUMN_TRAILER_ID + " TEXT NOT NULL, " +
-                MovieContract.TrailerEntry.COLUMN_ISO_369_1 + " TEXT NOT NULL, " +
+                MovieContract.TrailerEntry.COLUMN_MOVIE_ID + " INTEGER, " +
+                MovieContract.TrailerEntry.COLUMN_TRAILER_ID + " TEXT, " +
+                MovieContract.TrailerEntry.COLUMN_ISO_369_1 + " TEXT, " +
                 MovieContract.TrailerEntry.COLUMN_KEY + " TEXT NOT NULL, " +
                 MovieContract.TrailerEntry.COLUMN_NAME + " TEXT NOT NULL, " +
                 MovieContract.TrailerEntry.COLUMN_SITE + " TEXT NOT NULL, " +
-                MovieContract.TrailerEntry.COLUMN_SIZE + " TEXT NOT NULL, " +
-                MovieContract.TrailerEntry.COLUMN_TYPE + " TEXT NOT NULL, " +
-                MovieContract.TrailerEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                MovieContract.TrailerEntry.COLUMN_SIZE + " TEXT, " +
+                MovieContract.TrailerEntry.COLUMN_TYPE + " TEXT, " +
+                MovieContract.TrailerEntry.COLUMN_DATE + " INTEGER, " +
 
                 // Set up the movie_id column as a foreign key to movie table.
                 " FOREIGN KEY (" + MovieContract.TrailerEntry.COLUMN_MOVIE_ID + ") REFERENCES " +
@@ -64,12 +64,12 @@ public class MovieDBContract extends SQLiteOpenHelper {
 
         final String SQL_CREATE_REVIEW_TABLE = "CREATE TABLE " + MovieContract.ReviewEntry.TABLE_NAME + " (" +
                 MovieContract.ReviewEntry._ID + " INTEGER PRIMARY KEY," +
-                MovieContract.ReviewEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
-                MovieContract.ReviewEntry.COLUMN_REVIEW_ID + " TEXT NOT NULL, " +
+                MovieContract.ReviewEntry.COLUMN_MOVIE_ID + " INTEGER, " +
+                MovieContract.ReviewEntry.COLUMN_REVIEW_ID + " TEXT, " +
                 MovieContract.ReviewEntry.COLUMN_AUTHOR + " TEXT NOT NULL, " +
                 MovieContract.ReviewEntry.COLUMN_CONTENT + " TEXT NOT NULL, " +
-                MovieContract.ReviewEntry.COLUMN_URL + " TEXT NOT NULL, " +
-                MovieContract.ReviewEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                MovieContract.ReviewEntry.COLUMN_URL + " TEXT, " +
+                MovieContract.ReviewEntry.COLUMN_DATE + " INTEGER, " +
 
                 // Set up the movie_id column as a foreign key to movie table.
                 " FOREIGN KEY (" + MovieContract.ReviewEntry.COLUMN_MOVIE_ID + ") REFERENCES " +
